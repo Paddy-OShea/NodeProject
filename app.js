5ca3b4d8e966af3e9f41aeb4ddd028c2f297edf2
Paul Miles <paul2508@mail.ru>
2023-01-15 14:49:38 +0300

    add changes

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("Hello! This is my 1st lesson on Node Js")
+//{
+//let a = 5;
+//console.log(a);
+
+//var nubmer = 6;
+//console.log(nubmer);
+//var nubmer = 11;
+//console.log(nubmer);
+//function getNumber () {
+//    var nubmer = 10;
+//    console.log(nubmer);
+//}
+//getNumber();
