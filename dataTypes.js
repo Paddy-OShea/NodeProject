9c9bf4e3153ef30f2f2efc77af1aa1db96470b27
Paul Miles <paul2508@mail.ru>
2023-01-18 13:35:38 +0300

    add lesson about data types

---
+++ dataTypes.js
@@ -0,0 +1,19 @@
+/*let number = 4;
+let string = 'hello';
+let boolean = true; //false
+let nullValue = null;
+let undefinedValue;
+let sym = Symbol ();*/
+
+const first = 'hello';
+const second = 'world';
+const result = first + " " + second;
+console.log(result);
+
+/*const numFirst = 1500;
+const numSecond = 0b11;*/
+
+const x = 3;
+const y = 5;
+const c = x + y;
+console.log(c);
